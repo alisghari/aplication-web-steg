458dec9039d68c046082f7f493d66c230c533d65
alisghari <alisghari66@gmail.com>
2021-01-29 19:51:01 +0100

    second commit

--- index.js
+++ index.js
@@ -3,12 +3,47 @@ const app = express()
 const port = 1254
 const path = require('path')
 app.use(express.static(__dirname + "/public"));
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname + "/public/home.html"));
-})
-app.get('/err',(req,res)=>{
-    res.sendFile(path.join(__dirname + "/public/err.html"));
-})
+const timeCheck = function (req, res, next) {
+  var temp = moment().format("a");
+  var time = moment().format("hhmm");
+  if (temp == "am") {
+    if ((parseInt(time) < 0900) || (parseInt(time) >= 1200)){
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/err.html"));
+      });
+    }else {
+      app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/home.html"));
+      });
+      app.get("/services", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/services.html"));
+      });
+      app.get("/aboutus", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/about.html"));
+      });
+    }
+    
+    next();
+  } else {
+    if (parseInt(time) > 0500) {
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/error.html"));
+      });
+    } else {
+      app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/home.html"));
+      });
+      app.get("/services", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/services.html"));
+      });
+      app.get("/about", (req, res) => {
+        res.sendFile(path.join(__dirname + "/public/about.html"));
+      });
+    }
+  }
+  next();
+};
+
 
 app.listen(port, () => {
   console.log(
